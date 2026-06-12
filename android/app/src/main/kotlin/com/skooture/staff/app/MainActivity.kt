bc39f3c4496caf13c0853d710cff5a985c7e04ba
Mohamed Ghaly <MohamedGhaly>
2026-06-12 04:40:54 +0300

    Fix: Revert package name to com.skooture.staff.app and update Firebase config

---
+++ android/app/src/main/kotlin/com/skooture/staff/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.skooture.staff.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
--- android/app/src/main/kotlin/com/skooture/teacher/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.skooture.teacher
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
